d232b6fba9d46f85aa7dd192132e721da461de55
plovepsc <plovepsc@gmail.com>
2026-04-29 11:52:47 +0600

    mongo success

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
     if (err) {
         console.log("error!! " + err)
     } else {
-      //  console.log("MongoDB Connection Successful")
+       console.log("MongoDB Connection Successful")
     }
 })
 
